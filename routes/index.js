b62bc274e8e6b8a1078b53bda4efd01928713710
Nwaigba66 <glorianwaigba@gmail.com>
2024-02-15 01:54:35 +0100

    created First API inside server.js, create the Express server

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+const routes = express.Router();
+
+routes.get('/status', AppController.getStatus);
+
+routes.get('/stats', AppController.getStats);
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+
+app.use('/', routes);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
