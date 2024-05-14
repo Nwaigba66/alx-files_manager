4e0267467c5d8ecf278540cf6f89289a43f36021
Nwaigba66 <your.email@glorianwaigba@gmail.com>
2024-05-14 13:21:51 +0200

    added extra files

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,42 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-const getStatus = (req, res) => {
-  res.status(200);
-  res.send({
-    redis: redisClient.isAlive(),
-    db: dbClient.isAlive(),
-  });
-};
-
-const getStats = async (req, res) => {
-  Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-    .then((data) => {
-      res.status(200);
-      res.send({ users: data[0], files: data[1] });
-    })
-    .catch((error) => {
-      throw new Error(error);
+/**
+ * AppController class
+ */
+class AppController {
+  /**
+   * Retrieves the status of redisClient and dbClient, sets the response status
+   * code to 200, and sends the status of the clients in a JSON format in the
+   * response.
+   *
+   * @param {Object} request - The request object.
+   * @param {Object} response - The response object.
+   * @return {JSON} A JSON object containing the status of redisClient and
+   * dbClient.
+   */
+  static getStatus(request, response) {
+    response.statusCode = 200;
+    response.send({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
-};
+  }
 
-const AppController = { getStatus, getStats };
-module.exports = AppController;
+  /**
+   * Retrieves statistics from database and sends results in response.
+   *
+   * @param {Object} request - the request object
+   * @param {Object} response - the response object
+   * @return {Promise} a Promise that resolves when the response is sent
+   */
+  static async getStats(request, response) {
+    response.statusCode = 200;
+    response.send({
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    });
+  }
+}
+
+export default AppController;
